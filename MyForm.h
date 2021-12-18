fcbddc3df0fa537b3a4ba2f0a972dc0054d91e28
Kirill Makarovsky <kirill-ksm@yandex.ru>
2021-12-18 23:19:45 +0300

    Добавьте файлы проекта.

---
+++ MyForm.h
@@ -0,0 +1,279 @@
+#pragma once
+#include "Expressions.h"
+#include <iostream>
+namespace FormsPoliz 
+{
+	Expressions* ex = nullptr;
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// —водка дл€ MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// ќсвободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+			delete ex;
+			ex = nullptr;
+		}
+	private: System::Windows::Forms::TextBox^ textBox1;
+	protected:
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::Label^ label5;
+	private: System::Windows::Forms::DataGridView^ dataGridView1;
+	private: System::Windows::Forms::Button^ button2;
+	private: System::Windows::Forms::Button^ button3;
+
+
+
+
+
+	private:
+		/// <summary>
+		/// ќб€зательна€ переменна€ конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// “ребуемый метод дл€ поддержки конструктора Ч не измен€йте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->button2 = (gcnew System::Windows::Forms::Button());
+			this->button3 = (gcnew System::Windows::Forms::Button());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// textBox1
+			// 
+			this->textBox1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->textBox1->Location = System::Drawing::Point(24, 58);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(389, 35);
+			this->textBox1->TabIndex = 0;
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label1->Location = System::Drawing::Point(39, 28);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(112, 27);
+			this->label1->TabIndex = 1;
+			this->label1->Text = L"Expresson";
+			// 
+			// button1
+			// 
+			this->button1->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button1->Location = System::Drawing::Point(429, 60);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(122, 32);
+			this->button1->TabIndex = 2;
+			this->button1->Text = L"Enter";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label2->Location = System::Drawing::Point(22, 130);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(129, 27);
+			this->label2->TabIndex = 3;
+			this->label2->Text = L"Polsk invers";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label3->Location = System::Drawing::Point(21, 255);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(73, 27);
+			this->label3->TabIndex = 4;
+			this->label3->Text = L"Result";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label4->Location = System::Drawing::Point(19, 166);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(0, 27);
+			this->label4->TabIndex = 5;
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Font = (gcnew System::Drawing::Font(L"Times New Roman", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->label5->Location = System::Drawing::Point(19, 295);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(0, 27);
+			this->label5->TabIndex = 6;
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
+			this->dataGridView1->AutoSizeColumnsMode = System::Windows::Forms::DataGridViewAutoSizeColumnsMode::Fill;
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::ControlLightLight;
+			this->dataGridView1->ColumnHeadersVisible = false;
+			this->dataGridView1->Font = (gcnew System::Drawing::Font(L"Arial", 12));
+			this->dataGridView1->Location = System::Drawing::Point(812, 28);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->ScrollBars = System::Windows::Forms::ScrollBars::Vertical;
+			this->dataGridView1->Size = System::Drawing::Size(224, 372);
+			this->dataGridView1->TabIndex = 2;
+			this->dataGridView1->CellContentClick += gcnew System::Windows::Forms::DataGridViewCellEventHandler(this, &MyForm::dataGridView1_CellContentClick);
+			// 
+			// button2
+			// 
+			this->button2->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 15.75F, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button2->Location = System::Drawing::Point(24, 220);
+			this->button2->Name = L"button2";
+			this->button2->Size = System::Drawing::Size(122, 32);
+			this->button2->TabIndex = 8;
+			this->button2->Text = L"Calculate";
+			this->button2->UseVisualStyleBackColor = true;
+			this->button2->Click += gcnew System::EventHandler(this, &MyForm::button2_Click);
+			// 
+			// button3
+			// 
+			this->button3->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 18, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(204)));
+			this->button3->Location = System::Drawing::Point(861, 406);
+			this->button3->Name = L"button3";
+			this->button3->Size = System::Drawing::Size(122, 32);
+			this->button3->TabIndex = 9;
+			this->button3->Text = L"Enter";
+			this->button3->UseVisualStyleBackColor = true;
+			this->button3->Click += gcnew System::EventHandler(this, &MyForm::button3_Click);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(1175, 541);
+			this->Controls->Add(this->button3);
+			this->Controls->Add(this->button2);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->label1);
+			this->Controls->Add(this->textBox1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^>(this->dataGridView1))->EndInit();
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void MyForm_Load(System::Object^ sender, System::EventArgs^ e) {
+	}
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		std::string str;
+		for (size_t i = 0; i < textBox1->Text->Length; ++i)
+		{
+			str += textBox1->Text[i];
+		}
+		ex = new Expressions(str);
+		if (label4->Text != nullptr)
+			delete label4->Text;
+		label4->Text = gcnew System::String(ex->getPoliz().c_str());
+		size_t size = ex->getTable().GetCount();
+		dataGridView1->ColumnCount = 2;
+		dataGridView1->RowCount = size + 1;
+		dataGridView1->Rows[0]->Cells[0]->Value = "Variable";
+		dataGridView1->Rows[0]->Cells[1]->Value = "Value";
+		dataGridView1->Rows[0]->Cells[1]->ReadOnly = true;
+		for (size_t i = 0; i < size + 1; ++i)
+		{
+			dataGridView1->Rows[i]->Cells[0]->ReadOnly = true;
+		}
+		TTable table = (ex->getTable());
+		
+		for (size_t i = 1; i < size + 1; ++i)
+		{
+			//std::cout << (*table)[0].getName().c_str();
+			String^ str2 = gcnew String(table[i - 1].getName().c_str());
+			dataGridView1->Rows[i]->Cells[0]->Value = str2;
+			dataGridView1->Rows[i]->Cells[1]->Value = System::Convert::ToString(table[i - 1].getValue());
+			delete str2;
+		}
+	}
+	private: System::Void button2_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		double res = ex->calculate();
+		if (label5->Text != nullptr)
+			delete label5->Text;
+		label5->Text = gcnew System::String(System::Convert::ToString(res));
+	}
+	private: System::Void dataGridView1_CellContentClick(System::Object^ sender, System::Windows::Forms::DataGridViewCellEventArgs^ e) 
+	{
+	}
+
+
+
+	private: System::Void button3_Click(System::Object^ sender, System::EventArgs^ e) 
+	{
+		size_t size = ex->getTable().GetCount();
+		for (size_t i = 1; i < size + 1; ++i)
+		{
+			ex->getTable()[i - 1].setValue(System::Convert::ToInt16(dataGridView1->Rows[i]->Cells[1]->Value));
+		}
+	}
+};
+}
